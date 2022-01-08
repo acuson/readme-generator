50d831dd2d64431cd8a8cef9f6bb26897107aaf7
Alyssa Cuson <acuson98@gmail.com>
2022-01-08 16:02:34 -0500

    finished writeToFile function

--- Develop/index.js
+++ Develop/index.js
@@ -78,10 +78,20 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Your README file has been written.')
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
